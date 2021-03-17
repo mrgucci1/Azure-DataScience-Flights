c46d427b32d0249918c32ff61b8f30f04c7407f3
BuildTools <unconfigured@null.spigotmc.org>
2021-03-17 00:02:22 -0500

    looking at distribution of delayed flights

--- flights_data_exploration.py
+++ flights_data_exploration.py
@@ -1,13 +1,25 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+pd.set_option('display.max_rows', 50)
+pd.set_option('display.max_columns', 50)
+pd.set_option('display.width', 1000)
 # Read in data into dataframe
 df_flights = pd.read_csv('flights.csv')
 df_flights.head()
-
-#Identify missing values
-print(df_flights.isnull().sum())
 
 #Fill missing values with 0 
 df_flights.DepDel15 = df_flights.DepDel15.fillna(0)
 
 #Check we solved the missing values
 print(df_flights.isnull().sum())
+
+#Describe the data for every column
+print(df_flights.describe())
+
+#Create histogran fir DepDelay and ArrDelay
+var_depdel = df_flights['DepDelay']
+plt.hist(var_depdel)
+plt.title('Distribution of Departure Delay')
+plt.xlabel('value')
+plt.ylabel('frequency')
+plt.show()
