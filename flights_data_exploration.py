93f2b0e41f6ddb696b63cb1ff0add39245e1b949
mrgucci1 <dalquist.keaton221@gmail.com>
2021-04-20 13:15:47 -0500

    removed statistical outliers from departure delay, added summary stats for arrival dealay and hist plot

--- flights_data_exploration.py
+++ flights_data_exploration.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy import stats
+import matplotlib.ticker as ticker
 pd.set_option('display.max_rows', 50)
 pd.set_option('display.max_columns', 50)
 pd.set_option('display.width', 1000)
@@ -19,12 +21,25 @@ print(df_flights.describe())
 
 #Get Stats on Depparture delay
 var = df_flights['DepDelay']
+print("Departure delay stats")
 min_val = var.min()
 max_val = var.max()
 mean_val = var.mean()
 med_val = var.median()
 mod_val = var.mode()[0]
-
+print('Minimum:{:.2f}\nMean:{:.2f}\nMedian:{:.2f}\nMode:{:.2f}\nMaximum:{:.2f}\n'.format(min_val,
+                                                                                        mean_val,
+                                                                                        med_val,
+                                                                                        mod_val,
+                                                                                        max_val))
+#Get Stats on arrival delay
+var = df_flights['ArrDelay']
+print("Arrival delay stats")
+min_val = var.min()
+max_val = var.max()
+mean_val = var.mean()
+med_val = var.median()
+mod_val = var.mode()[0]
 print('Minimum:{:.2f}\nMean:{:.2f}\nMedian:{:.2f}\nMode:{:.2f}\nMaximum:{:.2f}\n'.format(min_val,
                                                                                         mean_val,
                                                                                         med_val,
@@ -32,21 +47,40 @@ print('Minimum:{:.2f}\nMean:{:.2f}\nMedian:{:.2f}\nMode:{:.2f}\nMaximum:{:.2f}\n
                                                                                         max_val))
 																						
 																						
-#Create histogran fir DepDelay and ArrDelay
+#Create histogran for DepDelay and ArrDelay
 # Create a Figure
 fig = plt.figure(figsize=(10,4))
-var_depdel = df_flights['DepDelay']
-plt.hist(var_depdel, 500, density=1,alpha=.05,align='mid')
-plt.xlim(xmin=-50, xmax = 400)
+q_hi = df_flights['DepDelay'].quantile(0.95)
+q_low = df_flights['DepDelay'].quantile(0.05)
+#filter dataframe so we are excluding the highest 5% and lowest 5% of the data
+var_depdel = df_flights[(df_flights['DepDelay']<q_hi)&(df_flights['DepDelay'] > q_low)]['DepDelay']
+plt.hist(var_depdel, 150)
 plt.title('Distribution of Departure Delay')
 plt.xlabel('value')
 plt.ylabel('frequency')
 plt.grid(color='#95a5a6', linestyle='--', linewidth=2, axis='y', alpha=0.7)
-plt.xticks(rotation=90)
 # Add lines for the statistics
-plt.axvline(x=min_val, color = 'gray', linestyle='dashed', linewidth = 2)
+#plt.axvline(x=min_val, color = 'gray', linestyle='dashed', linewidth = 2)
 plt.axvline(x=mean_val, color = 'cyan', linestyle='dashed', linewidth = 2)
 plt.axvline(x=med_val, color = 'red', linestyle='dashed', linewidth = 2)
 plt.axvline(x=mod_val, color = 'yellow', linestyle='dashed', linewidth = 2)
-plt.axvline(x=max_val, color = 'gray', linestyle='dashed', linewidth = 2)
+#plt.axvline(x=max_val, color = 'gray', linestyle='dashed', linewidth = 2)
+plt.show()
+# Create a Figure
+fig = plt.figure(figsize=(10,4))
+q_hi = df_flights['ArrDelay'].quantile(0.95)
+q_low = df_flights['ArrDelay'].quantile(0.05)
+#filter dataframe so we are excluding the highest 5% and lowest 5% of the data
+var_depdel = df_flights[(df_flights['ArrDelay']<q_hi)&(df_flights['ArrDelay'] > q_low)]['ArrDelay']
+plt.hist(var_depdel, 150)
+plt.title('Distribution of Arrival Delay')
+plt.xlabel('value')
+plt.ylabel('frequency')
+plt.grid(color='#95a5a6', linestyle='--', linewidth=2, axis='y', alpha=0.7)
+# Add lines for the statistics
+#plt.axvline(x=min_val, color = 'gray', linestyle='dashed', linewidth = 2)
+plt.axvline(x=mean_val, color = 'cyan', linestyle='dashed', linewidth = 2)
+plt.axvline(x=med_val, color = 'red', linestyle='dashed', linewidth = 2)
+plt.axvline(x=mod_val, color = 'yellow', linestyle='dashed', linewidth = 2)
+#plt.axvline(x=max_val, color = 'gray', linestyle='dashed', linewidth = 2)
 plt.show()
